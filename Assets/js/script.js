a5afb20da8d0048f5a6cf2a42e88d8a90a92a8c5
Soul <sornwilliam@gmail.com>
2020-05-09 17:33:37 -0500

    added more apis to searchbtn

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,7 @@ var currentDate = moment().format("MM/D/YYYY");
 
 var apiKey = "36dfa1267a90680fd8c48244e2de4540";
 
-// SEARCH FUNCTIONS
+// SEARCH FUNCTION
 searchbtn.on("click", function(event){
     event.preventDefault();
     
@@ -23,16 +23,17 @@ searchbtn.on("click", function(event){
     newbtn.text(city);
     searchcol.append(newbtn);
 
+    // GETS INFO TO WEATHER 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(queryURL);
-        console.log(response)
-        console.log(response.weather);
-        console.log(response.weather[0].icon);
+        // console.log(queryURL);
+        // console.log(response)
+        // console.log(response.weather);
+        // console.log(response.weather[0].icon);
 
         // gets icon code
         var iconCode = response.weather[0].icon;
@@ -43,8 +44,30 @@ searchbtn.on("click", function(event){
         var tempF = (response.main.temp - 273.15) * 1.80 +32;
         $("#temp").text("Temp (F): " + tempF.toFixed(2));
         $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        $("#windspd").text("Wind Speeds: " + response.wind.speed + "MPH");
+        $("#windspd").text("Wind Speeds: " + response.wind.speed + " MPH");
 
+        //UV API
+        var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid="+ apiKey +"&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
+        // console.log(uvURL);
+
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(uv) {
+            // console.log(uv);
+            $("#uv").text("UV Index: " + uv.value);
+        });
+
+    });
+
+    // 5DAY API
+    var daysURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=" + apiKey;
+
+    $.ajax({
+        url: daysURL,
+        method: "GET"
+    }).then(function(data){
+        console.log(data);
     });
 
     
