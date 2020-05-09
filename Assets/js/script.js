6617fd46e971bba515afd5563a7de04ae0e47891
Soul <sornwilliam@gmail.com>
2020-05-09 13:37:31 -0500

    added more stuff to searchbtn in script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ var inputCity = $("#cityinput");
 var searchbtn = $("#searchbtn");
 var searchcol = $(".searchcol");
 
+var currentDate = moment().format("MM/D/YYYY");
 
 var apiKey = "36dfa1267a90680fd8c48244e2de4540";
 
@@ -9,29 +10,44 @@ var apiKey = "36dfa1267a90680fd8c48244e2de4540";
 searchbtn.on("click", function(event){
     event.preventDefault();
     
-    var input = inputCity.val();
-    console.log(input);
+    var city = inputCity.val();
+    console.log(city);
 
-    if(input === ""){
+    // Won't append a button if empty string
+    if(city === ""){
         return false;
     }
     
     var newbtn = $("<button>");
     newbtn.attr("id", "savedbtn");
-    newbtn.text(input);
+    newbtn.text(city);
     searchcol.append(newbtn);
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ input + "&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(queryURL);
+        console.log(response)
         console.log(response.weather);
+        console.log(response.weather[0].icon);
+
+        // gets icon code
+        var iconCode = response.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + iconCode + ".png";
+
+        $("#cityname").html(response.name + " " + currentDate + " " + "<img src='" + iconURL +"' alt='icon for current weather'>");
+
+        var tempF = (response.main.temp - 273.15) * 1.80 +32;
+        $("#temp").text("Temp (F): " + tempF.toFixed(2));
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#windspd").text("Wind Speeds: " + response.wind.speed + "MPH");
+
     });
 
-
+    
 });
 
 
